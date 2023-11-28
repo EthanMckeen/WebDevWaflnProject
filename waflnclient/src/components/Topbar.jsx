bcb5c43c406bc1b7f0da4c8fcc14395ab88e3365
EthanMckeen <101148661+EthanMckeen@users.noreply.github.com>
2023-11-28 02:47:20 -0800

    New init
    have a start to backend working on front

---
+++ waflnclient/src/components/Topbar.jsx
@@ -0,0 +1,43 @@
+import "./topbar.css"
+
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ChatIcon from '@mui/icons-material/Chat';
+
+export default function Topbar(){
+    return(
+        <div className="topbarContainer">
+            <div className="topbarLeft">
+                <span className="logo">Wafln</span>
+            </div>
+            <div className="topbarCenter">
+                <div className="searchbar">
+                    <SearchIcon/>
+                    <input placeholder="Search for..." className="searchInput" />
+                </div>
+            </div>
+            <div className="topbarRight">
+                <div className="topbarLinks">
+                    <span className="topbarLink">Homepage</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <PersonIcon/>
+                        <span className="topbarIconBadge">3</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <ChatIcon/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <NotificationsIcon/>
+                        <span className="topbarIconBadge">5</span>
+                    </div>
+                </div>
+                <img src="assets\profilePicture\u1.jpg" alt="" className="topbarImg" />
+            </div>
+        </div>
+    )
+}
